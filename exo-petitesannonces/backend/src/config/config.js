3b38726fb43f78aab722faddc8cc025b4dee943d
Pasquier Théo <theo.pasquier@studentfr.ch>
2026-01-29 16:42:23 +0100

    terminé

--- exo-petitesannonces/backend/src/config/config.js
+++ exo-petitesannonces/backend/src/config/config.js
@@ -9,16 +9,16 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST || "db",
+  user: process.env.MYSQL_USER || "appuser",
+  password: process.env.MYSQL_PASSWORD || "userpwd",
+  database: process.env.MYSQL_DATABASE || "db_annonces",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 module.exports = { pool, port };
 
